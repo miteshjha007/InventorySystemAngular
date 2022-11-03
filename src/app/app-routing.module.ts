91574342735da64d3f0b1216b2e121c4d08d4680
Mitesh <cool.miteshjha@gmail.com>
2022-11-03 21:46:07 +0530

    Implementing Product Detail Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'about', component: AboutComponent, children: [
+
+  //     { path: 'company', component: AboutCompanyComponent },
+  //     { path: 'me', component: AboutMeComponent }
+  //   ]
+  // },
+
+  // { path: '', component: HomeComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  // { path: '**', component: NoPageComponent },   //Wild Card Page (Default Page if page not available.)
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
