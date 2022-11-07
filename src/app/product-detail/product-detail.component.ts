9b5b3dc18820f4cde191f1f13474952094587660
Mitesh <cool.miteshjha@gmail.com>
2022-11-07 14:28:07 +0530

    Complete CRUD operation

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { ActivatedRoute } from '@angular/router';
+import { ApiServiceService } from '../Services/api-service.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  Pname: any;
+  constructor(private product: AppComponent, private route: ActivatedRoute,private api :ApiServiceService) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+    console.log(params.get('id'))
+    this.api.getProduct().subscribe({
+      next:(res)=>{
+        this.Pname = res.filter(function(x:any){
+          return x.id == params.get('id');
+        })[0];
+      }
+    })
+    //  this.product.getProducts().subscribe(c =>{
+    //     console.log(c);
+    //     this.contact = c;
+    // })   
+    });
 
-  ngOnInit(): void {
   }
-
 }
